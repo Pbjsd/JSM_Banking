26cac7d31c4a2360adcf810faabf01d809a72ba2
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2025-01-02 21:09:40 -0500

    Updated and added new font in the className so that the font can be used across the entire application

--- jsm_banking/app/layout.tsx
+++ jsm_banking/app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${inter.variable} antialiased`}
-      >
-        {children}
+        className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}
       </body>
     </html>
   );
