e3beddba359bfd387c2163c454d48e27e5cfb91c
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2025-01-22 15:51:00 -0500

    Render username to appear in welcome page

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -2,7 +2,14 @@
 const HeaderBox = ({ type = "title", title, subtext, user }: HeaderBoxProps) => {
   return (
     <div className="header-box">
-      HeaderBox 
+      <h1 className="header-box-title">
+        {title}
+        {type === 'greeting' && (
+          <span className="text-bankGradient">
+            &nbsp;{user}
+          </span>
+        )}
+      </h1> 
     </div>
   )
 }
