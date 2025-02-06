8e6837d24dc55503c28485b0c05ed094644eba34
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2025-02-06 09:03:54 -0500

    Created Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,22 @@
-import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
-
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image"; 
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Sandeep', lastName: 'Kay' }; 
+
   return (
-    <main>
-      SIDEBAR 
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+        </div>
+      </div> 
       {children}
     </main>
   );
